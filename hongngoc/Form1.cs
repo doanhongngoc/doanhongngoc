1063fdb7b9d9d7c06acb923d898141f0cb044514
doanhongngoc <doanhongngoc351@gmail.com>
2018-01-25 09:23:03 +0700

    DEMO

--- hongngoc/Form1.cs
+++ hongngoc/Form1.cs
@@ -15,5 +15,10 @@ namespace hongngoc
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
